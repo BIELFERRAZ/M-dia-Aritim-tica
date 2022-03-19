4ea0907dffbb0c512b7f9e95e2eec3f3cca03316
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-18 22:25:02 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double n1 ,n2 , n3 , media;
+
+Console.WriteLine("Média Aritmética de 3 números\n");
+
+Console.Write("Digite o primeiro número..: ");
+n1 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Digite o segundo número...: ");
+n2 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Digite o terceiro número..: ");
+n3 = Convert.ToDouble(Console.ReadLine()!);
+
+media = (n1 + n2 + n3) / 4; 
+
+Console.WriteLine($"Média: {media:N1}");
